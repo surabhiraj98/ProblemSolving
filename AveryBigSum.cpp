8171fc45ecf57d66cf7411947e86967dea32bb39
Surabhi Kumari <42448772+surabhiraj98@users.noreply.github.com>
2019-10-15 00:49:50 +0530

    Create AveryBigSum.cpp

---
+++ AveryBigSum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+        long long int n ,i, sum=0;
+        cin >> n;
+        for(i=0;i<n;i++){
+            long long int a;
+        cin >> a;
+        sum =sum + a;
+        }
+    cout << sum << endl;
+    return 0;
+        
+        }
